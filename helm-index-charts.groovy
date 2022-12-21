6d23075fac0bd6cce9d014835b8a34cfafceaafe
Volodymyr Kartsev <volodymyr_kartsev@epam.com>
2022-12-22 00:37:41 +0200

    RANCHER-345

--- helm-index-charts.groovy
+++ helm-index-charts.groovy
@@ -16,7 +16,7 @@ properties([
     ])
 ])
 
-def chartsRepositoryUrl = "${Constants.FOLIO_GITHUB_URL}/folio-helm-v2.git@RANCHER-345"
+def chartsRepositoryUrl = "${Constants.FOLIO_GITHUB_URL}/folio-helm-v2.git"
 List chartsForIndex
 
 ansiColor('xterm') {
@@ -26,7 +26,7 @@ ansiColor('xterm') {
                 sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
                     checkout([
                         $class           : 'GitSCM',
-                        branches         : [[name: "*/master"]],
+                        branches         : [[name: "*/RANCHER-345"]],
                         extensions       : scm.extensions + [[$class             : 'SubmoduleOption',
                                                             disableSubmodules  : false,
                                                             parentCredentials  : false,
