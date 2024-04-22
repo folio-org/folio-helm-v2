2e01d3316971329883d5bc8a24c5a0fdb4e5728d
Arsen <151620067+aatoyan@users.noreply.github.com>
2024-04-22 11:26:04 +0400

    RAMCHER-1333 change agent version (#96)

--- helm-index-charts.groovy
+++ helm-index-charts.groovy
@@ -20,7 +20,7 @@ def chartsRepositoryUrl = "${Constants.FOLIO_GITHUB_URL}/folio-helm-v2.git"
 List chartsForIndex
 
 ansiColor('xterm') {
-    node('jenkins-agent-java11') {
+    node('jenkins-agent-java17') {
         try {
             stage('Init') {
                 sshagent(credentials: [Constants.GITHUB_CREDENTIALS_ID]) {
